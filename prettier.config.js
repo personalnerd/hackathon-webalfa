6a5a4b3c93bdeac6cc6895325c1116a5aa079af0
Tarcisio Cavalcante <cisoxp@gmail.com>
2022-12-17 14:13:03 -0300

    1st commit; index basic html css layout

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  endOfLine: 'lf',
+};
